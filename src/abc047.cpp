3b2ec33d039b67a14c4f00b12fbe74d98e839803
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-07 08:53:03 +0900

    abc047A 未解決

--- src/abc047.cpp
+++ src/abc047.cpp
@@ -9,6 +9,15 @@
 using namespace std;
 
 int main(void){
-    //
+    int num[3];
+    cin >> num[0] >> num[1] >> num[2];
+    //わからん
+    for(int i=0;i<3;i++){
+        if(num[0]+num[1]+num[2] % num[i] == 0 && num[i]==(num[0]+num[1]+num[2])/2){
+            cout << "Yes" << endl;
+            break;
+        }
+    }
+    cout << "No" << endl;
     return 0;
 }
