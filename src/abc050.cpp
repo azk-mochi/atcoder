35e51b0d7a23691e468e3d9906d7958c1b83037c
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-07 08:58:25 +0900

    ABC050A

--- src/abc050.cpp
+++ src/abc050.cpp
@@ -9,6 +9,12 @@
 using namespace std;
 
 int main(void){
-    //
+    int a, b; char op;
+    cin >> a >> op >> b;
+    if(op=='+'){
+        cout << a+b << endl;
+    }else{
+        cout << a-b << endl;
+    }
     return 0;
 }
