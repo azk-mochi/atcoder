2575ec248f0d8af65c153090d0661cb2c5aa5590
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-06 22:46:51 +0900

    abc023A

---
+++ src/abc023.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <complex>
+#include <fstream>
+
+using namespace std;
+
+int main(void){
+    int x; cin >> x;
+    cout << (x%10) + (x/10) << endl;
+    return 0;
+}
