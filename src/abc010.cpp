3e248d7fc787aaeeab14ff3ef4e47baaa204c8dd
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-06 21:53:41 +0900

    abc010A

---
+++ src/abc010.cpp
@@ -0,0 +1,19 @@
+//
+//  abc010.cpp
+//  atcoder
+//
+//  Created by Koshikawa Kenichi on 2018/06/06.
+//  Copyright © 2018年 reishoku. All rights reserved.
+//
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int A(){
+    string s;
+    cin >> s;
+    cout << s + "pp" << endl;
+}
