3ee845910193edf45efe937b72db4369aab98b3d
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-07 07:57:54 +0900

    abc034A

---
+++ src/abc034.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <complex>
+#include <fstream>
+
+using namespace std;
+
+int main(void){
+    int x, y; cin >> x >> y;
+    cout << ((x>y) ? "Worse" : "Better" ) << endl;
+    return 0;
+}
