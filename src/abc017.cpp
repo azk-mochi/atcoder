ea72a2c272c05ed5fcacf8900c5b942784a3823d
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-06 22:20:12 +0900

    abc017A

---
+++ src/abc017.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <complex>
+#include <fstream>
+
+using namespace std;
+
+int main(void){
+    int s[3]; int e[3];
+    for(int i=0;i<3;i++){
+        cin >> s[i] >> e[i];
+    }
+    int sum = 0;
+    for(int i=0;i<3;i++){
+        sum += (s[i] / 10 ) * e[i];
+    }
+    cout << sum << endl;
+    return 0;
+}
