f431696f185fa4f755c92840c7ab079a3888f617
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-07 08:27:08 +0900

    abc039A

---
+++ src/abc039.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <complex>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int main(void){
+    int a, b, c; cin >> a >> b >> c;
+    cout << 2*((a*b)+(b*c)+(c*a)) << endl;
+    return 0;
+}
