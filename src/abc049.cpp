a8992ca716240bec507ce2de0264ced9a3020f49
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-07 08:57:09 +0900

    ABC049A

--- src/abc049.cpp
+++ src/abc049.cpp
@@ -9,6 +9,11 @@
 using namespace std;
 
 int main(void){
-    //
+    char c; cin >> c;
+    if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' ){
+        cout << "vowel" << endl;
+    }else{
+        cout << "consonant" << endl;
+    }
     return 0;
 }
