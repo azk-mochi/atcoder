c1e9382df84b68c61879180cb3fff0502035eab8
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-07 08:47:40 +0900

    abc046A

--- src/abc046.cpp
+++ src/abc046.cpp
@@ -9,6 +9,13 @@
 using namespace std;
 
 int main(void){
-    //
+    int a, b, c; cin >>a >> b >> c;
+    if(a!=b && b!=c && c!=a){
+        cout << "3" << endl;
+    }else if(a==b&&b==c&&c==a){
+        cout << "1" << endl;
+    }else{
+        cout << "2" << endl;
+    }
     return 0;
 }
