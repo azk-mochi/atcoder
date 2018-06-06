dbe1a8b7a59ba1cbe402ff9b6c6d3fef2a5bb52b
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-07 08:54:38 +0900

    ABC048A

--- src/abc048.cpp
+++ src/abc048.cpp
@@ -9,6 +9,10 @@
 using namespace std;
 
 int main(void){
-    //
+    string atcoder;
+    string s;
+    string contest;
+    cin >> atcoder >> s >> contest;
+    cout << "A" << s[0] << "C" << endl;
     return 0;
 }
