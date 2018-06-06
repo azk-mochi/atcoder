fd55134effcdbd14ade710a5d2e191c8befca68e
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-07 08:44:08 +0900

    abc044A

--- src/abc044.cpp
+++ src/abc044.cpp
@@ -9,6 +9,16 @@
 using namespace std;
 
 int main(void){
-    //
+    int n, k, x, y;
+    cin >> n >> k >> x >> y;
+    int sum=0;
+    for(int i=0;i<n;i++){
+        if(i >= k){
+            sum += y;
+        }else{
+            sum += x;
+        }
+    }
+    cout << sum << endl;
     return 0;
 }
