152429fcc0618193de0de84a423ef1eb817d2f12
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-07 08:59:58 +0900

    added files

--- open_webpage.sh
+++ open_webpage.sh
@@ -3,6 +3,6 @@
 
 #AtCoderのWebページを開く用のスクリプト
 
-for i in {41..50}; do open "https://abc0${i}.contest.atcoder.jp/tasks/abc0${i}_a"; done; exit;
+#for i in {41..50}; do open "https://abc0${i}.contest.atcoder.jp/tasks/abc0${i}_a"; done; exit;
 
-cd /Developer/dev/atcoder/src && for i in {41..50}; do cp template.cpp abc0${i}.cpp; done; exit;
+cd /Developer/dev/atcoder/src && for i in {51..60}; do cp template.cpp abc0${i}.cpp; open "https://abc0${i}.contest.atcoder.jp/tasks/abc0${i}_a"; done; exit;
