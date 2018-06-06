39929c0dad2ff0180c59bdd0713307ce94ad7338
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-06 22:04:26 +0900

    abc013A

---
+++ src/abc013.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <complex>
+#include <fstream>
+
+using namespace std;
+
+int main(void){
+    char x; cin >> x;
+    switch(x){
+        case 'A':
+            cout << "1" << endl;
+            break;
+        case 'B':
+            cout << "2" << endl;
+            break;
+        case 'C':
+            cout << "3" << endl;
+            break;
+        case 'D':
+            cout << "4" << endl;
+            break;
+        case 'E':
+            cout << "5" << endl;
+            break;
+    }
+    return 0;
+}
