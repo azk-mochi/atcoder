c7a863a6a5ed54a0d9daea2eb993e636e8e9f46b
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-06 22:53:41 +0900

    abc025A 未解決

---
+++ src/abc025.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <complex>
+#include <fstream>
+
+using namespace std;
+
+int main(void){
+    string s; int n;
+    cin >> s >> n;
+    //わからん
+    return 0;
+}
