f405336bb0a38e5427c669080dc650e4f7b5c63f
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-06 22:41:56 +0900

    abc021A未解決

---
+++ src/abc021.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <complex>
+#include <fstream>
+
+using namespace std;
+
+int main(void){
+    int n;
+    cin >> n;
+    //わからん
+    return 0;
+}
