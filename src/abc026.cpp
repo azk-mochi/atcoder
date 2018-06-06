d2cda5368c077575bd9968a3b18c678dfdf22495
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-06 22:56:54 +0900

    abc026A

---
+++ src/abc026.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <complex>
+#include <fstream>
+
+using namespace std;
+
+int main(void){
+    int a; cin >> a;
+    cout << pow(a,2)/4 << endl;
+    return 0;
+}
