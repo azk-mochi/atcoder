3b6c732b0a933e6dcad9ce45490ef90b91a31c36
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-07 11:23:53 +0900

    ABC077A 未解決

---
+++ src/abc077.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <complex>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int main(void){
+    //わからんのでパス
+    int c[2][3];
+    int d[2][3];
+    for(int i=0;i<2;i++){
+        for(int j=0;j<3;j++){
+            cin >> c[i][j];
+            c[i][j]=d[i][j];
+        }
+    }
+    
+    return 0;
+}
