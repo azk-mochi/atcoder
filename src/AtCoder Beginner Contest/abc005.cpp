3435a79b931e0e9299107909bf520e2d1db3640d
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-07 16:19:23 +0900

    ABC005B

--- src/AtCoder Beginner Contest/abc005.cpp
+++ src/AtCoder Beginner Contest/abc005.cpp
@@ -11,3 +11,14 @@ int A(){
     cin >> x >> y;
     cout << ( y / x ) << endl;
 }
+
+int main(void){
+    int n; cin >> n;
+    vector<int> t(n);
+    for(int i=0;i<n;i++){
+        cin >> t[i];
+    }
+    sort(t.begin(), t.end());
+    cout << t[0] << endl;
+    return 0;
+}
