e83987e20e893b4885fdb1ab3b0fc895195bbfb3
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-07 09:10:45 +0900

    ABC054A

--- src/abc054.cpp
+++ src/abc054.cpp
@@ -9,6 +9,22 @@
 using namespace std;
 
 int main(void){
-    //
+    int a, b; cin >> a >> b;
+    
+    if(a==1){
+        a += 13;
+    }
+    if(b==1){
+        b += 13;
+    }
+    
+    if(a==b){
+        cout << "Draw" << endl;
+    }else if(a<b){
+        cout << "Bob" << endl;
+    }else{
+        cout << "Alice" << endl;
+    }
+    
     return 0;
 }
