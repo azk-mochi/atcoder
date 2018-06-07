8f0b0f587ce9ac23510be7d0d355b92f8fe6c0f1
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-07 09:04:39 +0900

    abc051A 未解決

--- src/abc051.cpp
+++ src/abc051.cpp
@@ -6,9 +6,14 @@
 #include <fstream>
 #include <cmath>
 
+#include <stdio.h>
+
 using namespace std;
 
 int main(void){
-    //
+    //わからんのでパス
+    char s1[5]; char s2[7]; char s3[5];
+    scanf("%s,%s,%s",&s1, &s2, &s3);
+    printf("%s %s %s",s1, s2, s3);
     return 0;
 }
