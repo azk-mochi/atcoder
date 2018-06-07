31acd7982e9ab20aaedf83cfa7ce794aed97bc9e
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-07 16:17:45 +0900

    ABC004B 未解決

--- src/AtCoder Beginner Contest/abc004.cpp
+++ src/AtCoder Beginner Contest/abc004.cpp
@@ -11,3 +11,31 @@ int A(){
     cout << 2 * n << endl;
 }
 
+int main(void){
+    //あっている気がするのにぜんぶWrong Answer. よくわからない。
+    int arr[4][4];
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            cin >> arr[i][j];
+        }
+    }
+    int cparr[4][4];
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            cparr[i][j] =arr[3-i][3-j];
+        }
+    }
+    //show
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            if(j==0){
+                cout << cparr[i][j];
+            }
+            if(j==1 || j==2 || j==3){
+                cout << " " << cparr[i][j];
+            }
+            cout << endl;
+        }
+    }
+    return 0;
+}
