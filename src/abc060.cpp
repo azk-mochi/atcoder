01d53d486db3ae5b6e6c14fc45e6c210d1ead99a
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-07 09:24:56 +0900

    ABC060A

--- src/abc060.cpp
+++ src/abc060.cpp
@@ -9,6 +9,12 @@
 using namespace std;
 
 int main(void){
-    //
+    string a, b, c;
+    cin >> a >> b >> c;
+    if((a.back() == b.front()) && b.back() == c.front()){
+        cout << "YES" << endl;
+    }else{
+        cout << "NO" << endl;
+    }
     return 0;
 }
