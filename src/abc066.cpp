13d4b65a945bbf5adabd454277017726af045271
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-07 11:02:45 +0900

    ABC066A

--- src/abc066.cpp
+++ src/abc066.cpp
@@ -9,6 +9,9 @@
 using namespace std;
 
 int main(void){
-    //
+    vector<int >arr(3);
+    cin >> arr[0] >> arr[1] >> arr[2];
+    sort(arr.begin(),arr.end());
+    cout << arr[0] + arr[1] << endl;
     return 0;
 }
