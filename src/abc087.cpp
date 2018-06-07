07f5cf98d8ff28e3755268fea44059bea27f7faf
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-07 11:51:38 +0900

    ABC087A

---
+++ src/abc087.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <complex>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int main(void){
+    int x, a, b;
+    cin >>x >> a >> b;
+    cout << (x-a)%b << endl;
+    return 0;
+}
