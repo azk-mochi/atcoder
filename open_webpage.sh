9ebbd88bee30d255351ad7f52eebdbecbecbd6d1
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-07 15:42:57 +0900

    ABC001B

--- open_webpage.sh
+++ open_webpage.sh
@@ -3,6 +3,6 @@
 
 #AtCoderのWebページを開く用のスクリプト
 
-#for i in {41..50}; do open "https://abc0${i}.contest.atcoder.jp/tasks/abc0${i}_a"; done; exit;
+for i in {1..9}; do open "https://abc00${i}.contest.atcoder.jp/tasks/abc00${i}_2"; done; exit;
 
-cd /Developer/dev/atcoder/src && for i in {81..98}; do cp template.cpp abc0${i}.cpp; open "https://abc0${i}.contest.atcoder.jp/tasks/abc0${i}_a"; done; exit;
+#cd /Developer/dev/atcoder/src && for i in {81..98}; do cp template.cpp abc0${i}.cpp; open "https://abc0${i}.contest.atcoder.jp/tasks/abc0${i}_a"; done; exit;
--- src/AtCoder Beginner Contest/abc001.cpp
+++ src/AtCoder Beginner Contest/abc001.cpp
@@ -6,4 +6,28 @@ using namespace std;
 int A(){
     int h1, h2; cin >> h1 >> h2;
     cout << h1 - h2 << endl;
+    return 0;
+}
+
+int main(void){
+    int m;
+    cin >> m;
+    if(m<100){
+        cout << "00" << endl;
+    }else if(100<=m && m <=5000){
+        //3桁me-toru
+        if(m<1000){
+            cout << "0" << (m/100) << endl;
+        }else
+        //4桁m
+        /*if(1000<=m)*/{
+            cout << (m/100) << endl;
+        }
+    }else if(6000<=m && m <= 30000){
+        cout << (m/1000) + 50 << endl;
+    }else if(35000<=m && m<=70000){
+        cout << 80 + (((m/1000) - 30)/5) << endl;
+    }else if(70000<m){
+        cout << "89" << endl;
+    }
 }
