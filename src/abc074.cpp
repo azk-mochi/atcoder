a4a80f1c7fe827c4e6c67af61a0c6b052aa3f7de
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-07 11:11:15 +0900

    ABC074 さっきのは間違えてABC071のファイルに書いてしまった

---
+++ src/abc074.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <complex>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int main(void){
+    int n, a; cin >> n >> a;
+    cout << pow(n,2) - a << endl;
+    return 0;
+}
