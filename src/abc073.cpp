59d8c126ce67182fe670d3d52792aed2efee9c1b
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-07 11:18:18 +0900

    ABC073A

---
+++ src/abc073.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <complex>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int main(void){
+    int n; cin >> n;
+    if(n%10==9 || n/10==9){
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+    }
+    return 0;
+}
