73adfa97aceb3e349f524033f05555a67fd3749f
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-07 11:21:19 +0900

    ABC076A

---
+++ src/abc076.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <complex>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int main(void){
+    int r, g;
+    cin >> r >> g;
+    cout << 2*g - r << endl;
+    return 0;
+}
