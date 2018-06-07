14b10d4f2927065d82e11c0a8100ce4585103108
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-07 11:14:28 +0900

    ABC072A

---
+++ src/abc072.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <complex>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int main(void){
+    int x, t;
+    cin >> x >> t;
+    cout << x-t << endl;
+    return 0;
+}
