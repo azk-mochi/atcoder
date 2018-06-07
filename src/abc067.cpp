63fe06a2c613b08d88b8b9202f74d56c0f058df5
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-07 11:05:25 +0900

    ABC067A

--- src/abc067.cpp
+++ src/abc067.cpp
@@ -9,6 +9,8 @@
 using namespace std;
 
 int main(void){
-    //
+    int a, b;
+    cin >> a >> b;
+    cout << (((a+b) % 3 == 0 || a%3==0 || b%3==0 ) ? "Possible" : "Impossible" ) << endl;
     return 0;
 }
