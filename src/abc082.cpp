cf2fded78980d60c550408f2e9fbb44efd0a75f0
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-07 11:41:13 +0900

    ABC082A

---
+++ src/abc082.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <complex>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int main(void){
+    //わからないのでパス
+    int a, b; cin >> a >> b;
+    cout << ceil((int)((double)(a+b)/2)) << endl;
+    return 0;
+}
