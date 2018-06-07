2a025f224f71589cb423809a6ccded525b06fc94
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-07 11:54:14 +0900

    ABC089A

---
+++ src/abc089.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <complex>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int main(void){
+    int n; cin >> n;
+    cout << n/3 << endl;
+    return 0;
+}
