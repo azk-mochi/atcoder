9d89625f642b26876a2d1639d5385317dcbbd313
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-07 14:32:21 +0900

    ABC095A

--- src/abc095.cpp
+++ src/abc095.cpp
@@ -2,13 +2,20 @@
 #include <vector>
 #include <algorithm>
 #include <string>
-#include <complex>
-#include <fstream>
 #include <cmath>
 
 using namespace std;
 
 int main(void){
+    string s;
+    cin >> s;
+    int sum = 700;
+    for(int i=0;i<3;i++){
+        if(s[i]=='o'){
+            sum += 100;
+        }
+    }
+    cout << sum << endl;
+    
     return 0;
 }
-
