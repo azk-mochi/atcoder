e926723eb9c31b34f8cccbf2af70a97604e8adb5
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-07 09:33:24 +0900

    ABC064A

--- src/abc064.cpp
+++ src/abc064.cpp
@@ -9,6 +9,12 @@
 using namespace std;
 
 int main(void){
-    //
+    int r, g, b;
+    cin >> r >> g >> b;
+    if((100*r+10*g+b)%4==0){
+        cout << "YES" << endl;
+    }else{
+        cout << "NO" << endl;
+    }
     return 0;
 }
