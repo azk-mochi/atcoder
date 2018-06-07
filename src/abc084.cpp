fce014d5ce4bd2c71e376b32fb0bc0d3de0681ce
Koshikawa Kenichi <kenichi.koshikawa.8f@stu.hosei.ac.jp>
2018-06-07 11:46:12 +0900

    ABC084A

---
+++ src/abc084.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <complex>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int main(void){
+    int m;
+    cin >> m;
+    cout << 24 + (24-m) << endl;
+    return 0;
+}
